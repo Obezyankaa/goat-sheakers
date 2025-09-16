bd47074b055cb23d46be69921c224090035b7e06
Vladislav Voloshanovskiy <voloshanowky@yandex.ru>
2025-09-16 18:37:15 +0400

    start type-script

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,33 @@
 import { watch, ref, provide, computed } from 'vue';
 import Drawer from './components/Drawer.vue';
 import Header from './components/Header.vue';
+import type { Item } from './types/item';
+import { CartKey } from './types/cart'
 
-const cart = ref<Item>([])
+const cart = ref<Item[]>([])
 const drawerOpen = ref(false)
 
-const totalPrice = computed(() =>
-  cart.value?.reduce((acc, curr) => acc + (curr?.price || 0), 0)
-)
-const vatPrice = computed(() => Math.round((totalPrice.value * 5) / 100))
 
-const closeDrawer = () => {
+// переписать функции для прайсво и передать в компонент корзины
+// const totalPrice = computed(() =>
+//   cart.value?.reduce((acc, curr) => acc + (curr?.price || 0), 0)
+// )
+// const vatPrice = computed(() => Math.round((totalPrice.value * 5) / 100))
+
+const closeDrawer = (): void => {
   drawerOpen.value = false
 }
 
-const openDrawer = () => {
+const openDrawer = (): void => {
   drawerOpen.value = true
 }
 
-const addToCart = (item) => {
-  console.log(item);
-  // cart.value.push(item)
+const addToCart = (item: Item): void => {
+  cart.value.push(item)
   item.isAdded = true
 }
 
-const removeFromCart = (item) => {
+const removeFromCart = (item: Item): void => {
   cart.value.splice(cart.value.indexOf(item), 1)
   item.isAdded = false
 }
@@ -38,14 +41,13 @@ watch(
   { deep: true },
 )
 
-provide('cart', { cart, closeDrawer, openDrawer, addToCart, removeFromCart })
+provide(CartKey, { cart: cart?.value, closeDrawer, openDrawer, addToCart, removeFromCart })
 </script>
 
 <template>
-  <Drawer v-if="drawerOpen" v-auto-animate :total-price="totalPrice" :vat-price="vatPrice" @create-order="createOrder"
-    :is-loading="isCreatingOrder" />
+  <Drawer v-if="drawerOpen" v-auto-animate @create-order="createOrder" :is-loading="isCreatingOrder" />
   <div class="bg-white w-4/5 m-auto rounded-xl mt-14">
-    <Header :total-price="totalPrice" @open-drawer="openDrawer" />
+    <Header @open-drawer="openDrawer" />
     <button @click="addToCart"></button>
     <div class="p-10">
       <router-view></router-view>
--- src/types/cart.ts
+++
@@ -1,8 +0,0 @@
-export interface Item {
-  id: number
-  title: string
-  price: number
-  imageUrl: string
-  favoriteId?: null | string
-  isAdded?: boolean
-}
